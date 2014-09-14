a5f649489fa1ed97088231861c055fd5bcaf7be1
Christie Hua <christiehua@gmail.com>
2014-09-14 11:17:42 -0700

    citylocation serializer

--- app/models/citylocation.rb
+++ app/models/citylocation.rb
@@ -1,16 +1,5 @@
 class Citylocation < ActiveRecord::Base
 
-  def self.party(limit, search, category)
-
-		limit ||= 10
-
-		auth = { query: { pid: 'uid4689-25055367-8', limit: limit, fl: 'd', fts: search, cat: category}} # Adds to end of URL ?apikey=<YOURKEY>&q=<MOVIE>
-		search_url = "http://api.shopstyle.com/api/v2/products"
-		
-		response = HTTParty.get search_url, auth
-
-		response.parsed_response["products"]
-  end
-	
+  
 
 end
--- app/serializers/citylocation_serializer.rb
+++ app/serializers/citylocation_serializer.rb
@@ -1,3 +1,3 @@
 class CitylocationSerializer < ActiveModel::Serializer
-  attributes :id, :airport, :month, :maxprice, :minprice, :priceamount
+  attributes :id, :airport, :month, :maxprice, :minprice, :priceamount, :origin, :destination, :lengthofstay, :maxfare
 end
