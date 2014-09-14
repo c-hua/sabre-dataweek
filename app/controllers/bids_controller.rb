ddf40284762115dbb31efc2b205a7fa7060f1909
Christie Hua <christiehua@gmail.com>
2014-09-14 11:55:29 -0700

    bid views

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,68 +1,74 @@
 class BidsController < ApplicationController
+  respond_to :json, :html
 
   def index
-  	@bids = Bid.find_by_sql('SELECT * FROM bids ORDER BY created_at DESC')
-  	 respond_to do |format|
-      	format.html # index.html.erb
-      	format.json { render json: @bids }
+    @bids = Bid.find_by_sql('SELECT * FROM bids ORDER BY created_at DESC')
+    respond_with @bids
   end
 
   def compindex
-  	@bids = Bid.find_by_sql('SELECT * FROM bids ORDER BY created_at DESC')
-  	 respond_to do |format|
-      	format.html # index.html.erb
-      	format.json { render json: @bids }
+    @bids = Bid.find_by_sql('SELECT * FROM bids ORDER BY created_at DESC')
+    respond_with @bids
   end
 
   def show
-  	@bid = Bid.find(params[:id])
+    @bid= Bid.find(params[:id])
   end
 
   def new 
-  	@bid = Bid.new
-    @location = cookies[:location]
+    @bid = Bid.new
   end
 
   def create
-    @location = cookies[:location]
-  	@bid = Bid.new(bid_params)
-  	if @bid.save(bid_params)
-  		redirect_to bids_path
-  	else
-  		render 'new'
-  	end
+    @bid = Bid.new(bid_params)
+
+    if @bid.save
+      respond_to do |format|
+        format.html { redirect_to bids_path }
+        format.json { render json: @bid, status: :created }
+      end
+    else
+      respond_to do |format|
+        format.html { render 'new' }
+        format.json { render json: @bid.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
-  	@bid = Bid.find(params[:id])
+    @bid = Bid.find(params[:id])
   end
 
   def update
-  	@bid = Bid.find(params[:id])
-  	if @bid.update(bid_params)
-  		redirect_to bids_compindex_path
-  	else 
-  		render 'edit'
-  	end
+    @bid = Bid.find(params[:id])
+    if @bid.update(bid_params)
+      redirect_to bids_compindex_path
+    else 
+      render 'edit'
+    end
   end
 
   def destroy
-    @bid= Bid.find(params[:id])
+    @bid = Bid.find(params[:id])
     @bid.destroy
     redirect_to bids_path
   end
 
-  def timecost
-  	@maxbid = cookies[:maxbid]
-  	# if @userbid <= @maxbid
-  		# render 'notification'
-  	# end
+  def matchbid
+    @bid = Bid.find(params[:id])
+    @maxfare = Citylocation.find(params[:maxfare])
+      if @bid <= @maxfare
+        render 'notification'
+      else
+        redirect to bids_compindex_path
+      end
   end
 
-  def 
 
-  
   def bid_params
     params.require(:bid).permit(:location, :date, :time, :maxbid)
+  end
+  def citylocation_params
+    params.require(:citylocation).permit(:airport, :month, :maxprice, :minprice, :priceamount, :origin, :destination, :lengthofstay, :maxfare)
   end
 end
